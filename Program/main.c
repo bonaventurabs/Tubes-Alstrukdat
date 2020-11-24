6bd930dc8adbb0008bc7ed009484cef3e152684d
bonaventurabs <bonaventurabagas.bb@gmail.com>
2020-11-24 19:41:49 +0700

    add main

---
+++ Program/main.c
@@ -0,0 +1,28 @@
+#include "../boolean/boolean.h"
+#include "../point/point.h"
+#include "../arrayDinamik/listDinamik.h"
+#include "../mesinkarakter/mesinkar.h"
+#include "../mesinkarakter/mesinkata.h"
+#include "../CircularQueue/circular_queue.h"
+#include "../Stack/stack.h"
+#include "../matriks/matriks.h"
+#include "../graf/graf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n");
+	printf("                               ____    _    __  _  _______  ____\n");
+	printf("                              |  __|  / \\  |  \\| ||__   __||    |\n");
+	printf("                              |____  / _ \\ | |\\  |   | |   | || |\n");
+	printf("                               |___|/_/ \\_\\|_| \\_|   |_|   |____|\n");
+	printf("                              --T.Y.C.O.O.N--  \n");
+	printf("\n");
+
+    printf("                                    Selamat datang di game\n");
+	printf("                                         SANTO TYCOON\n");
+    printf("                                         1. NEW GAME\n");
+    printf("                                         2. LOAD GAME\n");
+
+}
