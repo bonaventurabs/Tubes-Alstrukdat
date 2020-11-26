eb5189b12180edd29c8e65fc2e7f24787fce8002
afadhilr <56217321+afadhilr@users.noreply.github.com>
2020-11-26 21:20:23 +0700

    Update listDinamik.h
    add "Dynamic" in every list

--- Program/arrayDinamik/listDinamik.h
+++ Program/arrayDinamik/listDinamik.h
@@ -10,62 +10,62 @@ typedef struct {
     ElType *A;
     int Capacity;
     int Neff;
-} List;
+} DynamicList;
 
 /**
  * Konstruktor
  * I.S. sembarang
  * F.S. Terbentuk List kosong dengan ukuran InitialSize
  */
-List MakeList();
+DynamicList MakeList();
 
 /**
  * Destruktor
  * I.S. List terdefinisi
  * F.S. list->A terdealokasi
  */
-void DeallocateList(List *list);
+void DeallocateList(DynamicList *list);
 
 /**
  * Fungsi untuk mengetahui apakah suatu list kosong.
  * Prekondisi: list terdefinisi
  */
-boolean IsEmpty(List list);
+boolean IsEmpty(DynamicList list);
 
 /**
  * Fungsi untuk mendapatkan banyaknya elemen efektif list, 0 jika tabel kosong.
  * Prekondisi: list terdefinisi
  */
-int Length(List list);
+int Length(DynamicList list);
 
 /**
  * Mengembalikan elemen list L yang ke-I (indeks lojik).
  * Prekondisi: list tidak kosong, i di antara 0..Length(list).
  */
-ElType Get(List list, IdxType i);
+ElType Get(DynamicList list, IdxType i);
 
 /**
  * Fungsi untuk mendapatkan kapasitas yang tersedia.
  * Prekondisi: list terdefinisi
  */
-int GetCapacity(List list);
+int GetCapacity(DynamicList list);
 
 /**
  * Fungsi untuk menambahkan elemen baru di index ke-i
  * Prekondisi: list terdefinisi, i di antara 0..Length(list).
  */
-void InsertAt(List *list, ElType el, IdxType i);
+void InsertAt(DynamicList *list, ElType el, IdxType i);
 
 /**
  * Fungsi untuk menambahkan elemen baru di akhir list.
  * Prekondisi: list terdefinisi
  */
-void InsertLast(List *list, ElType el);
+void InsertLast(DynamicList *list, ElType el);
 
 /**
  * Fungsi untuk menambahkan elemen baru di awal list.
  * Prekondisi: list terdefinisi
  */
-void InsertFirst(List *list, ElType el);
+void InsertFirst(DynamicList *list, ElType el);
 
 #endif	
